55720a51072efd762c6a0657e81278f928151a9b
vaxo <vaxojaniashvili186@gmail.com>
2024-03-07 01:35:35 +0400

    everything-is-finished-on-markup

--- src/components/Inputs/FirstInput.tsx
+++ src/components/Inputs/FirstInput.tsx
@@ -1,10 +1,14 @@
+import logo from "../../images/send.svg";
 const FirstInput = () => {
   return (
     <div className="inputOne">
-    <h1>User 1</h1>
-    <input type="text"/>
-  </div>
-  )
-}
+      <h1>User 1</h1>
+      <div style={{display:"flex",position:"relative"}}>
+        <input type="text" />
+        <img style={{cursor:"pointer"}} className="send" src={logo} alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default FirstInput;
--- src/components/Inputs/SecondInput.tsx
+++ src/components/Inputs/SecondInput.tsx
@@ -1,9 +1,14 @@
+import logo from "../../images/send.svg";
+
 const SecondInput = () => {
   return (
-    <div className="inputTwo">
-    <h1>User 2</h1>
-    <input type="text"/>
-  </div>
+    <div className="inputOne">
+      <h1 style={{marginTop:15}}>User 1</h1>
+      <div style={{display:"flex",position:"relative"}}>
+        <input type="text" />
+        <img style={{cursor:"pointer"}} className="send" src={logo} alt="" />
+      </div>
+    </div>
   )
 }
 
